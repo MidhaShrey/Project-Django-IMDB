2deca74146090e25694d7eb11f2cedcf761bb039
Shrey Midha <admin@MacBook-Air.local>
2026-05-28 22:58:49 +0530

    Added Status Codes

--- watchlist_app/api/serializers.py
+++ watchlist_app/api/serializers.py
@@ -2,7 +2,7 @@ from watchlist_app.models import Movie
 from rest_framework import serializers
 
 class MovieSerializer(serializers.Serializer):
-    id = serializers.IntegerField(read_only=True)
+    id = serializers.IntegerField(read_only=True) # It is added to make the id field read-only, so it cannot be modified by the user
     name = serializers.CharField()
     description = serializers.CharField()
     isActive = serializers.BooleanField()
@@ -10,6 +10,7 @@ class MovieSerializer(serializers.Serializer):
     def create(self, validated_data):
         return Movie.objects.create(**validated_data)
     
+    # here instance holds old values and validated_data holds new values, so we need to update the instance with new values and save it to the database
     def update(self, instance, validated_data):
         instance.name = validated_data.get('name', instance.name)
         instance.description = validated_data.get('description', instance.description)
--- watchlist_app/api/views.py
+++ watchlist_app/api/views.py
@@ -1,5 +1,6 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework import status
 from watchlist_app.models import Movie
 from watchlist_app.api.serializers import MovieSerializer
 
@@ -8,17 +9,21 @@ def movie_list(request):
     if request.method == 'GET':
         movies = Movie.objects.all()
         serializer = MovieSerializer(movies, many=True) # when we have multiple objects, we need to set many=True
-        return Response(serializer.data)
+        return Response(serializer.data, status = status.HTTP_200_OK)
     elif request.method == 'POST':
         serializer = MovieSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors) 
+            return Response(serializer.data, status = status.HTTP_201_CREATED)
+        else:
+            return Response(serializer.errors,status = status.HTTP_400_BAD_REQUEST) 
 
 @api_view(['GET','PUT','DELETE'])
 def movie_details(request, primary_key):
-    movie = Movie.objects.get(pk=primary_key)
+    try:
+        movie = Movie.objects.get(pk=primary_key)
+    except Movie.DoesNotExist:
+        return Response({'error': 'Movie not found'}, status=status.HTTP_404_NOT_FOUND)
     if request.method == 'GET':
         serializer = MovieSerializer(movie)
         return Response(serializer.data)
@@ -26,8 +31,9 @@ def movie_details(request, primary_key):
         serializer = MovieSerializer(movie, data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors)
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     elif request.method == 'DELETE':
         movie.delete()
-        return Response(status=204)
+        return Response(status=status.HTTP_204_NO_CONTENT)
