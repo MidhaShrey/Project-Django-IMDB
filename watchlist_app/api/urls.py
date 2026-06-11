2aba31a745dfb2bdaaae9ad2f7235b673fe15317
Shrey Midha <admin@MacBook-Air.local>
2026-06-11 14:15:09 +0530

    Updated URLs

--- watchlist_app/api/urls.py
+++ watchlist_app/api/urls.py
@@ -4,11 +4,11 @@ from watchlist_app.api.views import *
 # Class Based Views
 urlpatterns = [
     path('list/', MovieListAV.as_view(), name='movie-list'),
-    path('<int:primary_key>', MovieDetailsAV.as_view(), name='movie-details'),
+    path('detail/<int:primary_key>/', MovieDetailsAV.as_view(), name='movie-details'),
 ]
 
 # Function Based Views
 # urlpatterns = [
 #     path('list/', movie_list, name='movie-list'),
-#     path('<int:primary_key>', movie_details, name='movie-details'),
+#     path('detail/<int:primary_key>/', movie_details, name='movie-details'),
 # ]
