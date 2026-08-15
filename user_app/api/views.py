9025c293df3591b029a0885a98f89029728c712d
MidhaShrey <midhashrey@gmail.com>
2026-08-15 15:50:40 +0530

    Added Token Auth - Registration - In Progress

---
+++ user_app/api/views.py
@@ -0,0 +1,13 @@
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+from user_app.api.serializers import RegistrationSerializer
+
+@api_view(['POST',])
+def registration_view(request):
+    if request.method == 'POST':
+        serializer = RegistrationSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({'message': 'User registered successfully.'}, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
