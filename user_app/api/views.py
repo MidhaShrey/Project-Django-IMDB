7a2fe3f9d753c3248acc6017d1a7a8a82ac28179
MidhaShrey <midhashrey@gmail.com>
2026-08-18 23:22:42 +0530

    Added Models.py and modified views.py

--- user_app/api/views.py
+++ user_app/api/views.py
@@ -1,13 +1,30 @@
+from rest_framework.authtoken.models import Token
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
 from user_app.api.serializers import RegistrationSerializer
 
+# Function-based view for user registration
 @api_view(['POST',])
 def registration_view(request):
     if request.method == 'POST':
         serializer = RegistrationSerializer(data=request.data)
+
+        data = {}
+
         if serializer.is_valid():
-            serializer.save()
-            return Response({'message': 'User registered successfully.'}, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+            account = serializer.save()
+
+            data['response'] = "Registration successful!"
+            data['username'] = account.username
+            data['email'] = account.email
+
+            token, _ = Token.objects.get_or_create(user=account)
+            data['token'] = token.key
+
+            return Response(data, status=status.HTTP_201_CREATED)
+        
+        else:
+            data = serializer.errors
+        
+        return Response(data, status=status.HTTP_400_BAD_REQUEST)
